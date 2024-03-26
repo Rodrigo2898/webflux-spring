d0484848ae4a13a9ed2a522e13a0e33411209077
Rodrigo2898 <rodrigofeitosarodrigues@gmail.com>
2024-03-26 13:51:42 -0300

    Ajustes nas configurações

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 gradle clean
-gradle botJar
-java -jar build/lib/reactive-flashcards-dio-1.0.0.jar
+gradle bootJar
+java -jar build/libs/reactive-flashcards-dio-1.0.0.jar
 #java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar build/lib/reactive-flashcards-dio-1.0.0.jar
